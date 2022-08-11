c798600add4fd98044a35f025a3c33d7a8b2de41
roxas <roxas@FERDINAND>
2022-08-11 23:44:30 +0800

    New Update

---
+++ LibrarrySystem/Controllers/MaintenanceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarrySystem.Controllers
+{
+    public class MaintenanceController : Controller
+    {
+
+        private readonly ILogger<MaintenanceController> _logger;
+
+        public MaintenanceController(ILogger<MaintenanceController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
--- LibrarrySystem/Program.cs
+++ LibrarrySystem/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Maintenance}/{action=Login}/{id?}");
 
 app.Run();
